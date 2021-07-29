366c8190741c422a4a79cb0b45ac286246ea8471
Glitch (early-elastic-bramble) <none>
2021-07-29 21:36:27 +0000

    👕🏃 Checkpoint ./Oyunlar/coinflip.js:34055447/41

--- Oyunlar/coinflip.js
+++ Oyunlar/coinflip.js
@@ -50,7 +50,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**<@${message.author.id}>spent cowoncy ${-kaybettin}<a:emoji_8:870059060714700870> and chose heads
-The coin spins...<a:emoji_8:870059060714700870> tail and you lost it all...50000-**`);
+The coin spins...<a:emoji_8:870059060714700870> tail and you lost it all...loose**`);
             }, 3000);
           });
       await db.set(`bahisoynama_${message.author.id}`, Date.now());   
@@ -62,7 +62,7 @@ The coin spins...<a:emoji_8:870059060714700870> tail and you lost it all...50000
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**<@${message.author.id}> spent cowoncy ${kazandın}<a:emoji_10:870059089055592508> and chose heads
-The coin spins...<a:emoji_10:870059089055592508> coinflip 100000+**`);
+The coin spins...<a:emoji_10:870059089055592508> coinflip win**`);
             }, 3000);
           });
     await db.set(`bahisoynama_${message.author.id}`, Date.now());   
