f3a17577d6b19555afde94cdcd751d1c7cc4663f
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-09-16 09:35:24 +0300

    Update cash.js

--- Ekonomi/cash.js
+++ Ekonomi/cash.js
@@ -9,11 +9,11 @@ exports.run = async (client, message, args) => {
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   var toplam= cüzdan+banka
 message.channel.send(new Discord.MessageEmbed()
-                  .setColor("YELLOW")
-                  .setFooter('Kraken Balance Info!')
+                  .setColor("RANDOM")
+                  .setFooter('MoonGame Balance Info!')
                   .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/871313810441510922/image0.gif')
                   .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
-                  .addField(`Kraken Cash <a:emoji_56:874712610396844135> `,`__${toplam ? toplam +'':`0`}__`,true))
+                  .addField(`MoonGash Cash  `,`__${toplam ? toplam +'':`0`}__`,true))
                     
   }
 
