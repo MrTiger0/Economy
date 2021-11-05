64db0a0e44121d61d550be42d056a7a86fd39a18
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-05 04:53:12 +0300

    Update coinflip.js

--- Oyunlar/coinflip.js
+++ Oyunlar/coinflip.js
@@ -46,7 +46,8 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
      if (awnser === "LOOSELOOSE") {
  var kaybettin = miktar*1       
         //////        
-        var text2 = `**<@${message.author.id}> CoinFlip <a:emoji_52:878634024271224872> Choice win Or Lost**`;
+        var text2 = `**<@${message.author.id}> CoinFlip <a:emoji_52:878634024271224872> Choice win Or Lost and chose **heads** 
+the coin spins...**`;
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`<@${message.author.id}>spent <:emoji_75:905991272370696202> ${-kaybettin}  and chose **heads**
