ab3a1e8050fa6d38285c573be13fbbbb2e001f3c
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-05 15:11:59 +0300

    Update para-sil.js

--- Ekonomi/para-sil.js
+++ Ekonomi/para-sil.js
@@ -7,29 +7,29 @@ let user = message.mentions.users.first()
 let money = args[1]    
   if(message.author.id !== ayarlar.botsahibi) return message.react("❌")
   if(!user) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`You must label the person to whom money will be removed from your wallet.!`))
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(!args[1]) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`⛔ You must enter the amount of money to be deleted from the wallet!`)) 
   
 if(isNaN(args[1])) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`⛔ You must enter the amount of money to be deleted from the wallet!`)) 
  
 if(args[1] < 0) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`🤔 The amount you entered is not a valid number !?`)
                                                    );    
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\  
  
 message.channel.send(new Discord.MessageEmbed()
-.setColor("GREEN")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`✅ ${user} from user's wallet ${money} 💸 deleted!`))
 db.add(`para_${user.id}`,- money)  
