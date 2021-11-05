dd2a437cd8245e0e26a9ea7310f7b6cf21dcfe84
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-05 15:13:34 +0300

    Update send.js

--- Ekonomi/send.js
+++ Ekonomi/send.js
@@ -9,34 +9,34 @@ module.exports.run = async (client, message, args) => {
   let  miktar = args[1]
   
   if(!etiket) return message.channel.send(new Discord.MessageEmbed()
-                      .setColor("RED")
+                      .setColor("36d8ff")
                       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))                   
                       .setDescription(`You must tag a user to send money!`))
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(! miktar) return message.channel.send(new Discord.MessageEmbed()
-                      .setColor("RED")
+                      .setColor("36d8ff")
                       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
                       .setDescription(`You must enter the amount of money you want to send!
                      \`send! <quantity || all>\``))
   if(miktar < 0 ||  miktar.startsWith('0') ) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+.setColor("36d8ff")//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(` The amount you entered is not a valid number !?`));
  if(miktar === 'all' || miktar === 'all') {
    if(para < 0) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(` You don't have any money now.`))//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 db.add(`para_${etiket.id}`, para)
 db.add(`para_${message.author.id}`, -para)   
 message.channel.send(new Discord.MessageEmbed()
-.setColor("GREEN")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(` Successful, ${etiket}'a ${para} 💸 you sent!`))
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
  } else {
   if(isNaN(miktar)) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`The amount you entered is not a valid number !?`))
  } //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
@@ -46,14 +46,14 @@ message.channel.send(new Discord.MessageEmbed()
                       .setDescription(`You can't send money to yourself!`))
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
-                      .setColor("RED")
+                      .setColor("36d8ff")
                       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))                 
                       .setDescription(`<Now in your hand ${para ? "just " + para + " 💸 there is!": "there is no money!"} `))
 if(miktar === 'all' || miktar === 'all') {
   return;
 }  else { //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   message.channel.send(new Discord.MessageEmbed()
-                .setColor('GREEN')
+                .setColor('36d8ff')
                 .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/870484993980391424/image0.gif')
                 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))  
 .setDescription(`**__Payment Transferred!__**
