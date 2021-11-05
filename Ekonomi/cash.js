f5a91a3231c4dda4c871918bc9ee4764d8662a33
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-05 05:16:30 +0300

    Update cash.js

--- Ekonomi/cash.js
+++ Ekonomi/cash.js
@@ -13,7 +13,7 @@ message.channel.send(new Discord.MessageEmbed()
                   .setFooter('BOXBOT Balance Info!')
                   .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/871313810441510922/image0.gif')
                   .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
-                  .addField(`MoonGash Cash  `,`__${toplam ? toplam +'':`0`}__`,true))
+                  .addField(`BOX Cash  `,`__${toplam ? toplam +'':`0`}__`,true))
                     
   }
 
