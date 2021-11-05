c4abbbd19f4060bb4a96173738c39fb9d284f3ec
MrTiGER <76209414+MrTiger0@users.noreply.github.com>
2021-11-05 15:11:23 +0300

    Update para-ekle.js

--- Ekonomi/para-ekle.js
+++ Ekonomi/para-ekle.js
@@ -8,29 +8,29 @@ module.exports.run = async (client, message, args) => {
 let money = args[1]    
   if(message.author.id !== ayarlar.botsahibi) return message.react("❌")
   if(!user) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`You must label the person to add money to your wallet.!`))
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(!args[1]) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`⛔ You must enter the amount of money to be added to your wallet!`)) 
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(isNaN(args[1])) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`⛔ You must enter the amount of money to be added to your wallet!`)) 
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   if(args[1] < 0) return message.channel.send(new Discord.MessageEmbed()
-.setColor("RED")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`🤔 The amount you entered is not a valid number!?`)
                                                       );    
 
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
 message.channel.send(new Discord.MessageEmbed()
-.setColor("GREEN")
+.setColor("36d8ff")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`✅ ${user} to user's wallet ${money} 💸 added!`))
 db.add(`para_${user.id}`, money)  
